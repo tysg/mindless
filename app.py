f75e4c688c3f6e95c8c4e205a91760e636deb370
Tianyi Song <42670338+tysg@users.noreply.github.com>
2021-09-04 13:37:42 +0800

    Serve static files at web/build/*

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, escape, request
+import os
+from flask import Flask,  send_from_directory
+
+STATIC_PATH = os.path.join("web", "build")
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'any secret key'
 
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+@app.route("/")
+def index():
+    return send_from_directory(STATIC_PATH, "index.html")
+
+
+@app.route("/<path:name>")
+def download_file(name):
+    return send_from_directory(
+        os.path.join("web", "build"), name
+    )
