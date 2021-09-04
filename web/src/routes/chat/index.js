6ba587e597a4cd46befba53c100868ff556b8650
anqichen9856 <chenanqi9856@gmail.com>
2021-09-04 21:01:09 +0800

    Change css and js

--- web/src/routes/chat/index.js
+++ web/src/routes/chat/index.js
@@ -10,14 +10,17 @@ import { AvatarContext } from "../../components/app";
 const Chat = () => {
   const avatar = useContext(AvatarContext);
   const [text, setText] = useState("");
-  const [speech, setSpeech] = useState("Click me!");
+  const [speech, setSpeech] = useState("Click me ! 🌸");
   const [wobble, setWobble] = useState(0);
+  const [prefix, setPrefix] = useState("");
 
   const requestGibberish = () => {
     axios.get(backendEndpoint).then((value) => {
       console.log(value);
       setWobble(1);
       setSpeech(value.data);
+      setPrefix("");
+      setText("");
     });
   };
 
@@ -26,6 +29,7 @@ const Chat = () => {
       console.log(value);
       setWobble(1);
       setSpeech(value.data);
+      setPrefix(text);
     });
   };
 
@@ -36,7 +40,7 @@ const Chat = () => {
           Back
         </Link>
       </nav>
-      <div class={style.bubble}>{speech}</div>
+      <div class={style.bubble}><b>{prefix+" "}</b> {speech}</div>
       <div class={style.container}>
         {avatar !== null && (
           <img class={style.avatar} src={avatar} 
@@ -44,8 +48,8 @@ const Chat = () => {
           onAnimationEnd={() => setWobble(0)}
           wobble={wobble}></img>
         )}
-        <input class={style.textinput} onInput={(e) => setText(e.target.value)} placeholder="Type a few words...">{text}</input>
-        <button class={style.button} onClick={requestMoreGibberish}>Send</button>
+        <input class={style.textinput} onInput={(e) => setText(e.target.value)} placeholder="Type a few words..." value={text}></input>
+        <button class={style.button} onClick={requestMoreGibberish}>Send ✈️</button>
       </div>
     </div>
   );
