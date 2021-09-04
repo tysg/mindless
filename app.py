b03395dc9a064d3511e658dced5ddc0edfed6920
Tianyi Song <42670338+tysg@users.noreply.github.com>
2021-09-04 14:52:38 +0800

    Run as 0.0.0.0

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ def download_file(name):
     )
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT")) if os.getenv("PORT") else 5000
-    app.run(host='127.0.0.1', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
